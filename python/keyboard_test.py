6ce9d997ef8c7b1c26df5aa1449a8baee05e4732
andyvuong <andyv.42@gmail.com>
2016-04-19 16:43:13 -0500

    Add mouse example and broken keyboard example (see issue)

--- python/keyboard_test.py
+++ python/keyboard_test.py
@@ -2,28 +2,30 @@ from pymouse import PyMouse
 from pykeyboard import PyKeyboard
 
 from pymouse import PyMouseEvent
+from pykeyboard import PyKeyboardEvent
 
-def fibo():
-    a = 0
-    yield a
-    b = 1
-    yield b
-    while True:
-        a, b = b, a+b
-        yield b
-
-class Clickonacci(PyMouseEvent):
+class MoveHandler(PyMouseEvent):
     def __init__(self):
         PyMouseEvent.__init__(self)
-        self.fibo = fibo()
 
-    def click(self, x, y, button, press):
-        '''Print Fibonacci numbers when the left click is pressed.'''
-        if button == 1:
-            if press:
-                print(self.fibo.next())
-        else:  # Exit if any other mouse button used
-            self.stop()
+    def move(self, x, y):
+        print (x, y)
 
-C = Clickonacci()
-C.run()
+class KeyHandler(PyKeyboardEvent):
+    def __init__(self):
+        PyKeyboardEvent.__init__(self)
+
+    def tap(self, keycode, character, press):
+        if press:
+            print character
+
+# Note: Initializing the handler takes some time
+M = MoveHandler()
+print "Starting"
+M.run()
+
+'''
+K = KeyHandler()
+print "Starting"
+K.run()
+'''
